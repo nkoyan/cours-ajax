de5914ba1af3b9f1ff422c6d25dd0c7a085f5c66
Nkoyan <yoann.kergall@gmail.com>
2019-02-17 21:57:11 +0100

    Adding Axios

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Entity\PostLike;
+use App\Repository\PostLikeRepository;
 use App\Repository\PostRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,5 +20,49 @@ class PostController extends AbstractController
         return $this->render('post/index.html.twig', [
             'posts' => $repo->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/post/{id}/like", name="post_like")
+     */
+    public function like(Post $post, ObjectManager $manager, PostLikeRepository $postLikeRepository)
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            return $this->json([
+                'code' => 403,
+                'message' => 'Unauthorized'
+            ], 403);
+        }
+
+        if ($post->isLikedByUser($user)) {
+            $like = $postLikeRepository->findOneBy([
+                'post' => $post,
+                'user' => $user
+            ]);
+
+            $manager->remove($like);
+            $manager->flush();
+
+            return $this->json([
+                'code' => 200,
+                'message' => 'Like bien supprimé',
+                'likes' => $postLikeRepository->count(['post' => $post])
+            ], 200);
+        }
+
+        $like = new PostLike();
+        $like->setPost($post)
+            ->setUser($user);
+
+        $manager->persist($like);
+        $manager->flush();
+
+        return $this->json([
+            'code' => 200,
+            'message' => 'Like bien ajouté',
+            'likes' => $postLikeRepository->count(['post' => $post])
+        ], 200);
     }
 }
